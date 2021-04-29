08a3e357926785583227b59fc74ebdc5e8501073
syedamehataj <bsyedataj@gmail.com>
2021-04-29 11:06:55 +0000

    added single line comment

---
+++ commentslc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Adding two values
+((sum=25+35))
+#Print the result
+echo $sum
+
+# sub two value
+((sum=10527-2598))
+#print the result
+echo $sum
+
+# mul. three values
+(( 129*25*1075))
+#print the result
+echo $sum
+
