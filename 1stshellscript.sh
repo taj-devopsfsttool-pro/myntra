34b5f2968bdb2a47128744478ba7f9594b1bad15
Cloud User <ec2-user@ip-172-31-35-103.ap-south-1.compute.internal>
2021-04-29 08:52:15 +0000

    added fst commit

---
+++ 1stshellscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "hello guys"
+echo "GM/GA/GE"
+echo "today date is"
+date
+echo "clanedar is..."
+cal
+
---
+++ 2ndshellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "hello guys"
+set -x
+echo "GM/GA/GE"
+set +x
+echo "today date is"
+date
+echo "clanedar is..."
+cal
+
